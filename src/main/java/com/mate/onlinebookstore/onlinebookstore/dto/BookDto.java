a0065565758a87e1ed9f5c34c4d9430efc6f46a8
Andrew Hudzenko <s1mpleworkpost@gmail.com>
2024-01-14 00:22:06 +0200

    add controller, dto, mapper, exception, plugin to pom, etc

---
+++ src/main/java/com/mate/onlinebookstore/onlinebookstore/dto/BookDto.java
@@ -0,0 +1,18 @@
+package com.mate.onlinebookstore.onlinebookstore.dto;
+
+import java.math.BigDecimal;
+import lombok.Data;
+
+@Data
+public class BookDto {
+    private Long id;
+    private String title;
+    private String author;
+    private String isbn;
+    private BigDecimal price;
+    private String description;
+    private String coverImage;
+}
+
+
+
