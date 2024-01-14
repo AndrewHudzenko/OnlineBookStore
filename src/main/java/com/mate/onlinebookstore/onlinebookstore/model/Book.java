64ab1787cd2cdee0121e32b50e6f92a3d35e75ff
Andrew Hudzenko <s1mpleworkpost@gmail.com>
2024-01-15 00:11:55 +0200

    add liquibase, soft delete, new methods, etc

--- src/main/java/com/mate/onlinebookstore/onlinebookstore/model/Book.java
+++ src/main/java/com/mate/onlinebookstore/onlinebookstore/model/Book.java
@@ -10,12 +10,16 @@ import java.math.BigDecimal;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
 @Getter
 @Setter
 @ToString
-@Table(name = "books")
 @Entity
+@SQLDelete(sql = "UPDATE books SET is_deleted = true WHERE id=?")
+@Where(clause = "is_deleted=false")
+@Table(name = "books")
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -30,4 +34,6 @@ public class Book {
     private BigDecimal price;
     private String description;
     private String coverImage;
+    @Column(nullable = false)
+    private Boolean isDeleted = false;
 }
--- src/main/java/com/mate/onlinebookstore/onlinebookstore/service/impl/BookServiceImpl.java
+++ src/main/java/com/mate/onlinebookstore/onlinebookstore/service/impl/BookServiceImpl.java
@@ -4,6 +4,7 @@ import com.mate.onlinebookstore.onlinebookstore.dto.BookDto;
 import com.mate.onlinebookstore.onlinebookstore.dto.CreateBookRequestDto;
 import com.mate.onlinebookstore.onlinebookstore.exception.EntityNotFoundException;
 import com.mate.onlinebookstore.onlinebookstore.mapper.BookMapper;
+import com.mate.onlinebookstore.onlinebookstore.model.Book;
 import com.mate.onlinebookstore.onlinebookstore.repository.BookRepository;
 import com.mate.onlinebookstore.onlinebookstore.service.BookService;
 import java.util.List;
@@ -33,5 +34,21 @@ public class BookServiceImpl implements BookService {
     public BookDto findById(Long id) {
         return bookMapper.toDto(bookRepository.findById(id).orElseThrow(
                 () -> new EntityNotFoundException(String.format("Book with id %s not found", id))));
+    }
+
+    @Override
+    public BookDto update(Long id, CreateBookRequestDto bookRequestDto) {
+        if (findById(id) != null) {
+            Book newBook = bookMapper.toModel(bookRequestDto);
+            newBook.setId(id);
+            bookRepository.save(newBook);
+            return bookMapper.toDto(newBook);
+        }
+        return null;
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        bookRepository.deleteById(id);
     }
 }
