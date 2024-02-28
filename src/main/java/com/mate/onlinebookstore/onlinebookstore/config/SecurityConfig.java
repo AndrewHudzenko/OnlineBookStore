6ec1873a5bda13d042ed54d1b63ff43bfee1f3d3
Andrew Hudzenko PC <s1mpleworkpost@gmail.com>
2024-02-28 14:27:52 +0200

    add jwt validation (some bug with project starting)

--- src/main/java/com/mate/onlinebookstore/onlinebookstore/config/SecurityConfig.java
+++ src/main/java/com/mate/onlinebookstore/onlinebookstore/config/SecurityConfig.java
@@ -2,22 +2,26 @@ package com.mate.onlinebookstore.onlinebookstore.config;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
+import com.mate.onlinebookstore.onlinebookstore.security.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableMethodSecurity
 @RequiredArgsConstructor
 @Configuration
 public class SecurityConfig {
     private final UserDetailsService userDetailsService;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
     public PasswordEncoder getPasswordEncoder() {
@@ -37,6 +41,8 @@ public class SecurityConfig {
                                 .authenticated()
                 )
                 .httpBasic(withDefaults())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .userDetailsService(userDetailsService)
                 .build();
     }
